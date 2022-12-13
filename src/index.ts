2a5da26f69b954f7fbf769783555e569d3b6ec86
Luke Tchang <ltchang@stanford.edu>
2022-12-13 07:36:23 -0800

    v1.0.13-alpha

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ export function stringify(value: any): string {
 }
 
 export function parse(text: string): any {
+  if (text == undefined || text == null) {
+    throw new Error("Cannot stringify null or undefined");
+  }
+
   return JSON.parse(text, (_, value) => {
     if (typeof value === "string") {
       const m = value.match(/(-?\d+)n/);
